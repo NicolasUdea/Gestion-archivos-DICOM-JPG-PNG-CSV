fd59ab91fb01af8947b0f0b85d08e3ee5c5c59d8
NicolasUdea <nicolas.vargasf@udea.edu.co>
2023-11-04 19:27:30 -0500

    Primer punto hecho

--- DICOM.py
+++ DICOM.py
@@ -1,9 +1,10 @@
 import pydicom
 import os
 
-
 class DICOM:
     def __init__(self, path):
+        self._DICOM__metadata = None
+        self.patient_info_tags = ['PatientID', 'PatientName', 'PatientBirthDate', 'PatientSex']  # Define patient_info_tags here
         folders = os.listdir(path)
         file_names = []
         for folder in folders:
@@ -16,10 +17,10 @@ class DICOM:
         for file in file_paths:
             self.__ds.append(pydicom.dcmread(file))
         self.__metadata = []
-        for ds in self.__ds:
-            self.__metadata.append(ds.dir())
+        for i in self.__ds:
+            self.__metadata.append(i.dir())
 
-    def get_metadata(self, file=0):
+    def get_table_metadata(self, file=0):
         """Creates a list of metadata for each DICOM file, returned as a table."""
         table = ""
         iters = [iter(self._DICOM__metadata[file])] * 4
@@ -29,17 +30,41 @@ class DICOM:
             table += "\n"
         return table
 
-    # Método que identifique los campos que contiene información del paciente y los muestre en pantalla.
-    def get_patient_info(self, file=0):
-        """Displays fields containing patient information"""
-        patient_info = []
-        for element in self._DICOM__metadata[file]:
-            if 'Patient' in element:
-                patient_info.append(element)
-        return patient_info
+    def anonymize_patient_info(self, file=0):
+        """Prints patient information, replaces it with N/A, and saves the modified DICOM file."""
+        ds = self.__ds[file]
+        print("Patient Information:")
+        for tag in self.patient_info_tags:
+            if tag in ds:
+                print(f"{tag}: {ds.data_element(tag).value}")
+                if tag == 'PatientBirthDate':
+                    ds.data_element(tag).value = "19000101"  # Use a dummy date
+                elif tag == 'PatientSex':
+                    ds.data_element(tag).value = "O"  # Use 'O' (Other) for sex
+                else:
+                    ds.data_element(tag).value = ""  # Use empty string
+        ds.save_as(f"anon/anonymized_{file}.dcm")
+        print(f"Anonymized DICOM file saved as anonymized_{file}.dcm")
+
+    def anonymize_directory(self, input_dir, output_dir):
+        """Anonymizes all DICOM files in a directory and saves them to another directory."""
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        for root, dirs, files in os.walk(input_dir):
+            for file in files:
+                if file.endswith(".dcm"):
+                    ds = pydicom.dcmread(os.path.join(root, file))
+                    for tag in self.patient_info_tags:
+                        if tag in ds:
+                            if tag == 'PatientBirthDate':
+                                ds.data_element(tag).value = "19000101"  # Use a dummy date
+                            elif tag == 'PatientSex':
+                                ds.data_element(tag).value = "O"  # Use 'O' (Other) for sex
+                            else:
+                                ds.data_element(tag).value = ""  # Use empty string
+                    ds.save_as(os.path.join(output_dir, f"anonymized_{file}"))
+        print(f"All DICOM files in {input_dir} have been anonymized and saved to {output_dir}.")
 
 test = DICOM('datos/Sarcoma/')
-a = test.get_metadata(1)
-print(a)
-b = test.get_patient_info(1)
-print(b)
+test.anonymize_patient_info()
+test.anonymize_directory('datos/Sarcoma/', 'anon/')
